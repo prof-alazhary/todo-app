8599e73e271c673bd8705cf2efdf20b30777b569
Mohammed Al-Azhary <mohammad.alazhary@enichers.com>
2018-12-22 23:49:43 +0200

    unit test first stage

---
+++ test/factories/Todo.js
@@ -0,0 +1,18 @@
+module.exports = {
+    todo: {
+        task: 'my task'
+    },
+    todos: [{
+            task: 'my task1',
+            done: true
+        },
+        {
+            task: 'my task2',
+            done: false
+        },
+        {
+            task: 'my task3',
+            done: false
+        },
+    ]
+}
