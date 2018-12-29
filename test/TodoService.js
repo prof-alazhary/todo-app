ab75d1522ac8e126d8fd59c68740466bbba7b798
Mohammed Al-Azhary <mohammad.alazhary@enichers.com>
2018-12-29 22:04:17 +0200

    create first test case

--- test/TodoService.js
+++ test/TodoService.js
@@ -1,20 +1,30 @@
-var sinon = require('sinon');
-var chai = require('chai');
-var expect = chai.expect;
+global.Promise = require('bluebird');
+
 require('sinon-mongoose');
+var sinon = require('sinon'),
+    chai = require('chai'),
+    { expect } = chai.expect,
+    should = chai.should();
 
-var Todo = require('../models/Todo');
-var TodoService = require('../services/TodoService');
+var Todo = require('../models/Todo'),
+    TodoService = require('../services/TodoService'),
+    { todo, todos } = require('./factories/Todo');
 
-describe("Get all todos", function(){
+
+describe("Get all todos", function () {
     let sandBox;
-    before(()=>{
+    before(() => {
         sandBox = sinon.createSandbox();
     })
-    // Test will pass if we get all todos
-   it("should return all todos", function(done){
-       sandBox.stub(Todo,)
-       done()
-   });
+
+    it("should return all todos that related with user", function (done) {
+        const userId = '1234';
+        sandBox.stub(Todo, 'find').returns(todos);
+        const _todos = TodoService.select(userId);
+
+        _todos.should.be.an('array').that.be.deep.equal(todos);
+        Todo.find.restore();
+        done();
+    });
 
 });
