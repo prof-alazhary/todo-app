1c74a11e2af797afaf0bdc380dd5413fabea8ca0
Mohammed Al-Azhary <mohammad.alazhary@enichers.com>
2019-03-20 19:05:17 +0200

    add some comments

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,13 @@ const mongoose = require('mongoose'),
 
 let mongoURI = 'mongodb://localhost/' + db_name;
 
+
     if(process.env.NODE_ENV == 'production'){
+        
+        // eg: if you deploy the app in Heroku platform and got installed "mLab MongoDB" add-on
+        // then you can get the connection URI like that:
+        // https://devcenter.heroku.com/articles/mongolab#getting-your-connection-uri
+
         mongoURI = process.env.MONGODB_URI;
     }
 
